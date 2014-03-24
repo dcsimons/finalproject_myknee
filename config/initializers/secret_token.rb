0c50e729b9f887195ff98b8e1d7856f9e2b702ab
David Simons <dave.c.simons@gmail.com>
2014-03-24 09:18:01 -0700

    first commit and added pg and devise gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyKnee::Application.config.secret_key_base = '337946020ecf91521f8dc44a28a501ca3c480c92f71b70eefc373c915ad0146dd99a3f8fd9d7f3750ce200700579d9c8a0db7d63d072d4ea1b43eda00597ebcf'
